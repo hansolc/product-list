d0a55449580bc8d9c530a060c53c8193a7b6b235
hansolc <hsc080808@naver.com>
2025-01-15 15:46:03 +0900

    add logout func

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,17 +5,19 @@ import React from 'react'
 import { useRecoilValue } from 'recoil'
 import { container, header, list } from './Header.css'
 import PageContainer from '../page-container'
+import useLogout from '@/app/(auth)/hooks/useLogout'
 
 const Header = () => {
   const userInfo = useRecoilValue(userState)
+  const { logout } = useLogout()
 
   if (!userInfo) return null
   return (
     <header className={header}>
       <div className={container}>
         <p>{`Welcome ${userInfo.username} please search for product!`}</p>
-        <ul className={list} style={{ display: 'none' }}>
-          <li>logout</li>
+        <ul className={list}>
+          <li onClick={logout}>logout</li>
         </ul>
       </div>
     </header>
